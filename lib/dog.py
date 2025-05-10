95045997f3d284f0e7e502f0cd7e1fdda78309ad
Primrose <primrose.njoki@student.moringaschool.com>
2025-05-10 09:45:08 +0300

    instance

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
-class Dog:
-    # Class body goes here
+# class Dog:
+#     # Class body goes here
 
-    #Instance method definition
-    pass
+#     #Instance method definition
+#     def bark(self):
+#         print('Woof!')
+
+# fido=Dog()
+# fido.bark() #method
+# pass
+# def sit(self):
+#         print('The dog is sitting.')
+
+# cate=Dog()
+# cate.sit()
+class Dog:
+    def bark(self):
+        print("Woof!")
+        
+    def sit(self):
+        print("The dog is sitting.")
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
-class Person:
-    # Class body goes here
 
+
+#class Person:
+   # Class body goes here
+class Person: # defines a new call
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+#rose = Person() # creates an instance of the person class
+#rose.talk()#calls the method on that instance
+    
+#print("test")
+#class Person:
+    def walk(self):
+        print("The person is walking.")
+
+#susu = Person()
+#susu.walk()
